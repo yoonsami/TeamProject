d9032ed3fdbfe9bf7635d6a0c20a003d3f91797e
JeongYoonseong <smfgkgks123@gmail.com>
2023-12-05 14:22:35 +0900

    ㅋㅌㅊㅍ

--- Client/Yeopo_FSM.cpp
+++ Client/Yeopo_FSM.cpp
@@ -186,8 +186,6 @@ void Yeopo_FSM::State_Tick()
         break;
     }
 
-    Update_GroupEffectWorldPos(Get_Owner()->Get_Transform()->Get_WorldMatrix());
-
     if (m_iPreFrame != m_iCurFrame)
         m_iPreFrame = m_iCurFrame;
 }
