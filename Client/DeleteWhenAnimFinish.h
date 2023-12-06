8653da6c77ef75a3eb896ba4512bad2e3b6223d0
JeongYoonseong <smfgkgks123@gmail.com>
2023-12-06 21:57:33 +0900

    a

--- Client/DeleteWhenAnimFinish.h
+++ Client/DeleteWhenAnimFinish.h
@@ -11,6 +11,7 @@ public:
     virtual void Tick() override;
     virtual shared_ptr<MonoBehaviour> Copy_Script() override { return make_shared<DeleteWhenAnimFinish>(m_strAnimName,m_fWaitTime,m_bAnimLoop,m_fAnimSpeed); }
 
+    void Set_DissolveOption(_float fDissolveSpeed, shared_ptr<Texture> pTexture) { m_fDissolveSpeed = fDissolveSpeed; m_pDissolveTexture = pTexture; }
 
 private:
     wstring m_strAnimName;
@@ -18,9 +19,11 @@ private:
     _float m_fAnimSpeed = 1.f;
     _float m_fWaitTime = 0.f;
     _bool m_bDissolveWhenFinished = false;
-    shared_ptr<Texture> m_pTexture;
+    _float m_fDissolveSpeed = 0.f;
+    shared_ptr<Texture> m_pDissolveTexture;
 
     _bool m_bAnimFinished = false;
+    _float m_fAcc = 0.f;
 
 };
 
