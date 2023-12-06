7942e08dea4bc69e8d5d97c17161b020d024cb57
okchoi <smilezzgg@naver.com>
2023-12-06 16:20:08 +0900

    npc상호작용까지 조절

--- Client/UIInteraction.h
+++ Client/UIInteraction.h
@@ -25,8 +25,10 @@ private:
 private:
     weak_ptr<GameObject> m_pInteraction_Bg;
     weak_ptr<GameObject> m_pInteraction_Font;
+
     weak_ptr<GameObject> m_pDialog_Controller;
     weak_ptr<GameObject> m_pGachaController;
+    weak_ptr<GameObject> m_pShopController;
 
     weak_ptr<GameObject> m_pAccessObj;
 
