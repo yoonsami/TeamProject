055483a6159c22748ec356372319ec8367bd027c
GeonGeonGeonGeon <128566681+GeonGeonGeonGeon@users.noreply.github.com>
2023-12-15 17:27:49 +0900

    마따끄! 좋은인생이었따!

--- Engine/WeedScript.cpp
+++ Engine/WeedScript.cpp
@@ -5,23 +5,24 @@
 
 void WeedScript::Tick()
 {
-	if (Get_Owner()->Get_MeshRenderer())
-	{
-		m_fWindWeight += fDT * m_fWindWeightPM * m_fWindSpeed;
-		if (m_fWindWeight >= 1.f)
-		{
-			m_fWindWeight = 1.f;
-			m_fWindWeightPM = -1.f;
-		}
-		else if (m_fWindWeight <= 0.f)
-		{
-			m_fWindWeight = 0.f;
-			m_fWindWeightPM = 1.f;
-			m_Wind.x = Utils::Random_In_Range(-1.f, 1.f);
-			m_Wind.y = Utils::Random_In_Range(-1.f, 1.f);
-			m_Wind.z = Utils::Random_In_Range(-1.f, 1.f);
-		}
+	// 너의 틱은 죽어있다 아무 기능도 없기떄문이지
+	//if (Get_Owner()->Get_MeshRenderer())
+	//{
+	//	m_fWindWeight += fDT * m_fWindWeightPM * m_fWindSpeed;
+	//	if (m_fWindWeight >= 1.f)
+	//	{
+	//		m_fWindWeight = 1.f;
+	//		m_fWindWeightPM = -1.f;
+	//	}
+	//	else if (m_fWindWeight <= 0.f)
+	//	{
+	//		m_fWindWeight = 0.f;
+	//		m_fWindWeightPM = 1.f;
+	//		m_Wind.x = Utils::Random_In_Range(-1.f, 1.f);
+	//		m_Wind.y = Utils::Random_In_Range(-1.f, 1.f);
+	//		m_Wind.z = Utils::Random_In_Range(-1.f, 1.f);
+	//	}
 
-		Get_Owner()->Get_MeshRenderer()->SetVec4(0, _float4{  m_Wind.x, m_Wind.y, m_Wind.z, m_fWindWeight });
-	}
+	//	Get_Owner()->Get_MeshRenderer()->SetVec4(0, _float4{  m_Wind.x, m_Wind.y, m_Wind.z, m_fWindWeight });
+	//}
 }
