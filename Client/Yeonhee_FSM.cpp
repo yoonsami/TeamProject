e7947007109494c67fb4feed7284197cb0d0493e
Yijin Shin <49178058+YijinShin@users.noreply.github.com>
2023-12-24 17:24:53 +0900

    yeonhee 메테오 수정

--- Client/Yeonhee_FSM.cpp
+++ Client/Yeonhee_FSM.cpp
@@ -1361,6 +1361,7 @@ void Yeonhee_FSM::skill_300100()
 {
     if (Init_CurFrame(25))
         Add_Effect(L"YeonHee_300100_Install");
+
     if (m_iCurFrame >= 10 && m_iCurFrame <= 85)
     {
         if (!m_pCamera.expired())
