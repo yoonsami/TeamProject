e26db76a21cb069d1234ede6aac2098cf9adb211
JeongYoonseong <smfgkgks123@gmail.com>
2023-12-28 15:16:49 +0900

    Render에는 Shader에 바인딩하는거만 남겨둠

--- Engine/MeshEffect.cpp
+++ Engine/MeshEffect.cpp
@@ -135,30 +135,7 @@ void MeshEffect::Render()
 
     m_pShader->Push_GlobalData(Camera::Get_View(), Camera::Get_Proj());
    
-    if (!m_bIsLocked)
-    {
-		m_RenderParams.SetFloat(0, m_fLifeTimeRatio);
-		m_RenderParams.SetFloat(1, m_fCurrDissolveWeight);
-		m_RenderParams.SetFloat(2, m_fCurrRimLightIntensity);
-		m_RenderParams.SetFloat(3, m_fFadeOutRatio);
-        _float2 vTemp2;
-        _float4 vTemp4;
-		Bind_UpdatedColor_ToShader();
-		vTemp2 = _float2(m_vCurrTexUVOffset_Distortion.x, m_vCurrTexUVOffset_Distortion.y);
-		m_RenderParams.SetVec2(3, vTemp2);
-		vTemp4 = _float4(m_vCurrTexUVOffset_Op1.x, m_vCurrTexUVOffset_Op1.y, m_vCurrTexUVOffset_Op2.x, m_vCurrTexUVOffset_Op2.y);
-		m_RenderParams.SetVec4(0, vTemp4);
-		vTemp4 = _float4(m_vCurrTexUVOffset_Op3.x, m_vCurrTexUVOffset_Op3.y, m_vCurrTexUVOffset_Blend.x, m_vCurrTexUVOffset_Blend.y);
-		m_RenderParams.SetVec4(1, vTemp4);
-		vTemp4 = _float4(m_vCurrTexUVOffset_Overlay.x, m_vCurrTexUVOffset_Overlay.y, m_vCurrTexUVOffset_Dissolve.x, m_vCurrTexUVOffset_Dissolve.y);
-		m_RenderParams.SetVec4(2, vTemp4);
-		vTemp4 = _float4(m_UVTexRangeX.x, m_UVTexRangeX.y, m_UVTexRangeY.x, m_UVTexRangeY.y);
-		m_RenderParams.SetVec4(3, vTemp4);
-
-		// For. Bind Data 
-		m_pShader->Push_RenderParamData(m_RenderParams);
-
-    }
+	m_pShader->Push_RenderParamData(m_RenderParams);
 
     m_pShader->Push_LightData(CUR_SCENE->Get_LightParams());
 
