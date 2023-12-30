2c4e06080ac8168fd4dfeaa702763a1c4631e4c6
Yijin Shin <49178058+YijinShin@users.noreply.github.com>
2023-12-30 13:27:49 +0900

    create group effect tick 수정

--- Client/CreateGroupEffectScript.cpp
+++ Client/CreateGroupEffectScript.cpp
@@ -17,7 +17,20 @@ void CreateGroupEffectScript::Tick()
 		// create effect 
 		if (m_fHintEffectTimer > 7.f)
 		{
-			Add_GroupEffectOwner(m_wstrEffectTag, _float3(0.f, 0.f, 0.f), true, nullptr, false);
+			shared_ptr<GameObject> pGroupEffectOwnerObj = make_shared<GameObject>();
+			pGroupEffectOwnerObj->Set_TimeSlowed(false);
+			// For. Transform 
+			pGroupEffectOwnerObj->GetOrAddTransform();
+		
+			// For. GroupEffect component 
+			shared_ptr<GroupEffectOwner> pGroupEffect = make_shared<GroupEffectOwner>();
+			pGroupEffectOwnerObj->Add_Component(pGroupEffect);
+			pGroupEffectOwnerObj->Get_GroupEffectOwner()->Set_GroupEffectTag(m_wstrEffectTag);
+			
+			// TODO : add script 
+
+			// For. Add Effect GameObject to current scene
+			EVENTMGR.Create_Object(pGroupEffectOwnerObj);
 			m_fHintEffectTimer = 0.f;
 		}
 	}
@@ -26,39 +39,4 @@ void CreateGroupEffectScript::Tick()
 		EVENTMGR.Delete_Object(Get_Owner());
 		return;
 	}
-}
-
-void CreateGroupEffectScript::Add_GroupEffectOwner(const wstring& strSkilltag, _float3 vPosOffset, _bool usePosAs, shared_ptr<MonoBehaviour> pScript, _bool bTimeSlowed)
-{
-	shared_ptr<GameObject> pGroupEffectOwnerObj = make_shared<GameObject>();
-	pGroupEffectOwnerObj->Set_TimeSlowed(bTimeSlowed);
-	// For. Transform 
-	pGroupEffectOwnerObj->GetOrAddTransform();
-	if (!usePosAs)
-	{
-		_float4 vOwnerLook = m_pOwner.lock()->Get_Transform()->Get_State(Transform_State::LOOK);
-		vOwnerLook.Normalize();
-		_float4 vOwnerRight = m_pOwner.lock()->Get_Transform()->Get_State(Transform_State::RIGHT);
-		vOwnerRight.Normalize();
-		_float4 vOwnerUp = m_pOwner.lock()->Get_Transform()->Get_State(Transform_State::UP);
-		vOwnerUp.Normalize();
-		_float4 vOwnerPos = m_pOwner.lock()->Get_Transform()->Get_State(Transform_State::POS)
-			+ vOwnerRight * vPosOffset.x
-			+ vOwnerUp * vPosOffset.y
-			+ vOwnerLook * vPosOffset.z;
-		pGroupEffectOwnerObj->Get_Transform()->Set_State(Transform_State::POS, vOwnerPos);
-	}
-	else
-		pGroupEffectOwnerObj->Get_Transform()->Set_State(Transform_State::POS, _float4(vPosOffset, 1.f));
-	pGroupEffectOwnerObj->Get_Transform()->Set_Quaternion(Get_Transform()->Get_Rotation());
-
-	// For. GroupEffect component 
-	shared_ptr<GroupEffectOwner> pGroupEffect = make_shared<GroupEffectOwner>();
-	pGroupEffectOwnerObj->Add_Component(pGroupEffect);
-	pGroupEffectOwnerObj->Get_GroupEffectOwner()->Set_GroupEffectTag(strSkilltag);
-	if (nullptr != pScript)
-		pGroupEffectOwnerObj->Get_GroupEffectOwner()->Set_GroupEffectScript(pScript);
-
-	// For. Add Effect GameObject to current scene
-	EVENTMGR.Create_Object(pGroupEffectOwnerObj);
 }
